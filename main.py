72384fbb85c90f41ed318f4707b3eb34004d93f1
Ivan <kravets10000@gmail.com>
2022-11-25 09:43:07 +0300

    wrong input check

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
-game_map = [['x', 'x', '0'],
-            [' ', 'x', '0'],
-            [' ', ' ', 'x']]
+game_map = [[' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']]
 
 players = ('x', '0')
 
@@ -10,34 +10,40 @@ players = ('x', '0')
 def choose_the_winner():
     for line in game_map:
         if len(set(line)) == 1 and line[0] in players:
-            print(f"Строка. Победил {line[0]}")
-            break
+            return line[0]
     for i in range(3):
         if len({game_map[j][i] for j in range(3)}) == 1 and game_map[0][i] in players:
-            print(f"Столбец. Победил {game_map[0][i]}")
-            break
+            return game_map[0][i]
     if game_map[1][1] in players:
         if len({game_map[0][0], game_map[1][1], game_map[2][2]}) == 1 or len({game_map[0][2], game_map[1][1], game_map[2][0]}) == 1:
-            print(f"диагональ. Победил {game_map[1][1]}")
+            return game_map[1][1]
             
- 
-print(choose_the_winner())
+while True:
+    print('   0   1   2')
+    for index, line in enumerate(game_map):
+        print(index, end='  ')
+        print(*line, sep=' | ')
+        if index < 2:
+            print('  -----------')
 
-# while True:
-#     print('   0   1   2')
-#     for index, line in enumerate(game_map):
-#         print(index, end='  ')
-#         print(*line, sep=' | ')
-#         if index < 2:
-#             print('  -----------')
-
-#     user_raw, user_column = map(int, input().split())
-#     game_map[user_raw][user_column] = 'x'
-#     while True:
-#         bot_raw = random.randint(0,2)
-#         bot_column = random.randint(0,2)
-#         if game_map[bot_raw][bot_column] == ' ':
-#             game_map[bot_raw][bot_column] = '0'
-#             break
-#     choose_the_winner()
-#     break
+    try:
+        user_raw, user_column = map(int, input().split())
+        game_map[user_raw][user_column] = 'x'
+    except ValueError as _:
+        print('введите 2 числа через пробел')
+        continue
+    except IndexError as _:
+        print('введите числа от 0 до 2')
+        continue
+    if winner := choose_the_winner():
+        print(f'Поздравляю победил {winner}')
+        break
+    while True:
+        bot_raw = random.randint(0,2)
+        bot_column = random.randint(0,2)
+        if game_map[bot_raw][bot_column] == ' ':
+            game_map[bot_raw][bot_column] = '0'
+            break
+    if winner := choose_the_winner():
+        print(f'Поздравляю победил {winner}')
+        break
